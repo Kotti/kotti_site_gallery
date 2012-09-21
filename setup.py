0fa90877329de2335f4cbf29585c27105c0b314c
Andreas kaiser <disko@binary-punks.com>
2012-09-21 12:24:27 +0200

    PEP8, bump version.

--- setup.py
+++ setup.py
@@ -5,12 +5,12 @@ here = os.path.abspath(os.path.dirname(__file__))
 README = open(os.path.join(here, 'README.rst')).read()
 CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
 
-install_requires=[
+install_requires = [
     'Kotti >= 0.7',
 ]
 
 setup(name='kotti_site_gallery',
-      version='0.1.0b1',
+      version='0.1.0b2',
       description="Event collection for Kotti sites",
       long_description=README + '\n\n' + CHANGES,
       classifiers=[
@@ -31,10 +31,9 @@ setup(name='kotti_site_gallery',
       include_package_data=True,
       zip_safe=False,
       install_requires=install_requires,
-      message_extractors = { "kotti_site_gallery": [
-        ("**.py",   "lingua_python", None ),
-        ("**.pt",   "lingua_xml", None ),
-        ]},
+      message_extractors={"kotti_site_gallery": [
+          ("**.py", "lingua_python", None),
+          ("**.pt", "lingua_xml", None), ]},
       entry_points="""\
       [fanstatic.libraries]
       kotti_site_gallery = kotti_site_gallery:lib_kotti_site_gallery
