78925c0bb23d52a5eb83b4019938fd3336378d33
Marco Scheidhuber <j23d@jusid.de>
2013-01-03 17:13:33 +0100

    Use pytest fixtures instead of UnitTest classes.

--- kotti_site_gallery/tests/test_site.py
+++ kotti_site_gallery/tests/test_site.py
@@ -1,11 +1,8 @@
-from kotti.testing import UnitTestBase
-
-
-class TestSiteImagesMethods(UnitTestBase):
+class TestSiteImagesMethods:
     """
     Test methods which operate on the site's pictures.
     """
-    def setUp(self):
+    def set_up(self):
         from kotti_site_gallery.resources import Site
         self.site = Site('www.frankfurt.de', title=u"Frankfurt",
                          body=u"<b>Foo! The bar.</b>")
@@ -17,14 +14,16 @@ class TestSiteImagesMethods(UnitTestBase):
         self.emty_site = Site('www.frankfurt.de', title=u"Frankfurt",
                               body=u"<b>Heyho.</b>")
 
-    def test_get_icon(self):
+    def test_get_icon(self, db_session):
+        self.set_up()
         icon = self.site.get_icon()
         assert icon.name == 'foo'
 
         icon = self.emty_site.get_icon()
         assert icon is None
 
-    def test_get_pictures(self):
+    def test_get_pictures(self, db_session):
+        self.set_up()
         pictures = list(self.site.get_pictures())
         assert len(pictures) == 3
         assert pictures[2].name == 'cruz'
